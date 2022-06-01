03b9b6e9f7ed563ea4c0c9869e3dc2c0deff588c
Marlen <awwadmarlen@gmail.com>
2022-06-01 16:47:48 +0300

    UI - Part 1 - display logo and use connectButton from Moralis

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-
+import { ConnectButton } from "web3uikit";
+import logo from "./images/Moralis.png"
 
 const App = () => {
 
   return (
     <>
-     
+      <div className="header">
+        <div className="logo">
+          <img src={logo} alt="logo" height="50px" />
+          Sentiment
+        </div>
+        <ConnectButton />
+      </div>
+
+      <div className="instructions">
+        Where do you think these tokens are going? Up or Down?
+      </div>
     </>
   );
 };
