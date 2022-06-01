84d5865d56fca56afba4ff590b9d564dda820a7f
Marlen <awwadmarlen@gmail.com>
2022-06-01 17:04:16 +0300

    UI - Part 2 - Display coin with percentage color depending on the percentage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { ConnectButton } from "web3uikit";
-import logo from "./images/Moralis.png"
+import logo from "./images/Moralis.png";
+import Coin from "./components/Coin";
 
 const App = () => {
+
+  const [btc, setBtc] = useState(38);
+
 
   return (
     <>
@@ -17,6 +21,13 @@ const App = () => {
 
       <div className="instructions">
         Where do you think these tokens are going? Up or Down?
+      </div>
+      <div className="list">
+        <Coin 
+          perc={btc}
+          setPerc={setBtc}
+          token={"BTC"}
+        />
       </div>
     </>
   );
--- src/components/Coin.js
+++ src/components/Coin.js
@@ -2,13 +2,39 @@ import React, { useEffect, useState } from "react";
 import "./Coin.css";
 
 
-function Coin() {
+function Coin({perc, setPerc, token}) {
 
-
+  const [color, setColor] = useState();
+  useEffect(() => {
+    if(perc < 50){
+      setColor("#c43d08");
+    }else{
+      setColor("green");
+    }
+  }, [perc]);
 
   return (
     <>
-      
+      <div>
+        <div className="token">
+          {token}
+        </div>
+        <div className="circle" style={{
+              boxShadow: `0 0 20px ${color}`
+            }}>
+          <div 
+            className="wave" 
+            style={{
+              marginTop: `${100 - perc}%`,
+              boxShadow: `0 0 20px ${color}`,
+              backgroundColor: color
+            }}
+          ></div>
+          <div className="percentage">
+            {perc}%
+          </div>
+        </div>
+      </div>
     </>
   );
 }
